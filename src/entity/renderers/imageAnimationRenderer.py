9c540a781857e98d203a43f0f2e715e5f1ca62cb
Tyler Griffin <tgriffin@dstsystems.com>
2015-01-23 14:07:15 -0600

    adding some comments about what I want/need to do with animations

--- src/entity/renderers/imageAnimationRenderer.py
+++ src/entity/renderers/imageAnimationRenderer.py
@@ -1,3 +1,15 @@
+#TODO: create an "Animation" class.  there should be one animation class for every animation file loaded.
+    #each imageAnimationRenderer should have one to many animation class instances (animations)
+    #the animationLoader.loadAnimation() function should accept only an animation file name, and return only an animation instance
+    #the animationLoader should be able to handle fixed-frame-length animations, or animations with different lengths to display different frames.
+    #imageAnimationRenderer should be able to use different x and y offsets for different animations.
+    #each type of animated entity should have it's own extension of imageAnimationRenderer, rather than extending imageAnimationRenderer directly.
+        #for example, the tree class should not pass animation file paths to imageAnimationRenderer.  that information should be stored in treeAnimationRenderer, which should extend imageAnimationRenderer.
+    #each .animation file (and associated animation class instance) should have a name to make the animation-switching code more readable.
+        #ImageAnimationRenderer can create a map to associate the name to the index in it's "animations" array.
+
+#TODO: need to make shared resources between animation and imageRenderer instances static.  right now I'm loading the same images multiple times. 
+
 from renderer import Renderer
 import assetLoader
 import utils
