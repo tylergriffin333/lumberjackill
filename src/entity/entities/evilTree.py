81a1c1073759c2ef64c32f3bc0362730de05b606
Tyler Griffin <tgriffin@dstsystems.com>
2015-01-27 15:01:27 -0600

    removing EvilTreeImageAnimationRenderer

--- src/entity/entities/evilTree.py
+++ src/entity/entities/evilTree.py
@@ -1,12 +1,15 @@
 from posDimEntity import PosDimEntity
 from fallingEntity import FallingEntity
 from rectColliderDynamic import RectColliderDynamic
-from evilTreeImageAnimationRenderer import EvilTreeImageAnimationRenderer
+from imageAnimationRenderer import ImageAnimationRenderer
+from imageAnimation import ImageAnimation
 
-class EvilTree(PosDimEntity, EvilTreeImageAnimationRenderer, RectColliderDynamic, FallingEntity):
+class EvilTree(PosDimEntity, ImageAnimationRenderer, RectColliderDynamic, FallingEntity):
     def __init__(self, game, x, y):
         PosDimEntity.__init__(self, game, x, y, 1.07, 1.67)
-        EvilTreeImageAnimationRenderer.__init__(self, game.graphicsRenderer)#TODO: need to make all things shared between instances static
+        ImageAnimationRenderer.__init__(self, game.graphicsRenderer)#TODO: need to make all things shared between instances static
+        self.runningAnimation=ImageAnimation(game.graphicsRenderer, "evil_tree/running.animation")
+        self.curAnimation=self.runningAnimation
         FallingEntity.__init__(self, game, x, y)
         RectColliderDynamic.__init__(self, game.collisionSystem)
         self.speed=.0085
--- src/entity/renderers/evilTreeImageAnimationRenderer.py
+++
@@ -1,8 +0,0 @@
-from imageAnimationRenderer import ImageAnimationRenderer
-from imageAnimation import ImageAnimation
-
-class EvilTreeImageAnimationRenderer(ImageAnimationRenderer):
-    def __init__(self, graphicsRenderer):
-        ImageAnimationRenderer.__init__(self, graphicsRenderer)
-        self.runningAnimation=ImageAnimation(graphicsRenderer, "evil_tree/running.animation")
-        self.curAnimation=self.runningAnimation
