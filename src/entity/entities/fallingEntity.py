18daae999bd87ffa4090a7002186144274f5a455
Tyler Griffin <tgriffin@dstsystems.com>
2015-05-11 15:01:29 -0500

    continuing refactoring of jack. working on jumping.

--- src/entity/entities/fallingEntity.py
+++ src/entity/entities/fallingEntity.py
@@ -6,6 +6,11 @@ class FallingEntity(DynamicEntity):
         DynamicEntity.__init__(self, game, x, y)
         self.termVel=.01#max fall speed
         self.onGround=False
+    
+    def hitTop(self, otherCollider):#called if your top hits something else's bottom (you've hit your head on the "ceiling")
+        if self.yVel<0:#is rising
+            self.yVel=0#stop rising
+        #TODO: need to figure out why this isn't working.  I thought this function would fix the bug where jack's head temporarily sticks to the ceiling while his yVel decelerates over time due to gravity.
         
     def hitBottom(self, otherCollider):#called if your bottom hits something else's top (you've landed on the "ground"
         if self.yVel>0:#is falling
--- src/entity/entities/jack.py
+++ src/entity/entities/jack.py
@@ -25,16 +25,29 @@ class Jack(PosDimEntity, ImageAnimationRenderer, RectColliderDynamic, FallingEnt
         self.yVel=0
         self.maxSpeed=.008#can't run left or right faster than this
         self.walkSpeed=self.maxSpeed*.6
+        self.jumpSpeed=-.012#your speed in the upward direction when when you jump
         
         self.game.graphicsRenderer.jack=self
         
-    def updateFromInputs(self):
-        self.xVel=self.input.joyAxisX*self.maxSpeed
+    #def hitBottom(self, otherCollider):#called if your bottom hits something else's top (you've landed on the "ground"
+        #print(self.onGround)
+        #FallingEntity.hitBottom(self, otherCollider)
+        #print(self.onGround)
+        #print()
         
-        if self.input.right: self.xVel=self.maxSpeed
-        elif self.input.left: self.xVel=-self.maxSpeed
+    def updateFromInputs(self):
+        curMaxSpeed=self.walkSpeed
+        if self.input.running: curMaxSpeed=self.maxSpeed
+        
+        self.xVel=self.input.joyAxisX*curMaxSpeed
+        
+        if self.input.right: self.xVel=curMaxSpeed
+        elif self.input.left: self.xVel=-curMaxSpeed
+        
+        if self.input.jump and self.onGround:#TODO: why is self.onGround always false here?
+            self.yVel=self.jumpSpeed
         
     def run(self):
+        self.updateFromInputs()
         FallingEntity.run(self)
         ImageAnimationRenderer.run(self)
-        self.updateFromInputs()
